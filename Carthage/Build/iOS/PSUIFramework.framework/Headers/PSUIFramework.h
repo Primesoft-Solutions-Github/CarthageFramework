7d266fce2988ce27e0ad32af48dc47af35e537af
Primesoft-Solutions-Github <rpasala@primesoft.net>
2019-02-07 15:42:29 +0530

    minor carthage frame works

---
+++ Carthage/Build/iOS/PSUIFramework.framework/Headers/PSUIFramework.h
@@ -0,0 +1,19 @@
+//
+//  PSUIFramework.h
+//  PSUIFramework
+//
+//  Created by Ravi Kumar Pasala on 2/7/19.
+//  Copyright © 2019 Ravi Kumar Palasa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PSUIFramework.
+FOUNDATION_EXPORT double PSUIFrameworkVersionNumber;
+
+//! Project version string for PSUIFramework.
+FOUNDATION_EXPORT const unsigned char PSUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSUIFramework/PublicHeader.h>
+
+
